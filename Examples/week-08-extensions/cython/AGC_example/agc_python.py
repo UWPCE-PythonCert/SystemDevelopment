15ecfc5bc91f88d7add9ebd4c9d84268a3408ba8
Chris Barker <PythonCHB@gmail.com>
2015-05-26 16:34:46 -0700

    cleaned up ACG python code

--- Examples/week-08-extensions/cython/AGC_example/agc_python.py
+++ Examples/week-08-extensions/cython/AGC_example/agc_python.py
@@ -1,45 +1,46 @@
 #!/usr/bin/env python
 
 """
-Pure python (and not bery numpy smart) version of the Automatic Gain Control function
+Pure python (and not very numpy smart) version of the
+Automatic Gain Control function
 """
 
 import numpy as np
 
+
 def agc(nAGC, amp):
-   """
-   run an automatic gain control filter onver the input array
+    """
+    run an automatic gain control filter onver the input array
 
-   :param nAGC: width of window, number of elements.
-   :type nAGC: integer
+    :param nAGC: width of window, number of elements.
+    :type nAGC: integer
 
-   :param amp: input amplitude data
-   :type amp: 1-d numpy array of float32
-   
-   :returns ampAGC: a numpy array of the filtered data.
+    :param amp: input amplitude data
+    :type amp: 1-d numpy array of float32
 
-   """
+    :returns ampAGC: a numpy array of the filtered data.
 
-   #make sure input array is as expected:
-   amp = np.asarray(amp, dtype=np.float32)
-   if len(amp.shape) != 1:
-       raise ValueError("amp must be a rank-1 array")   
-   
-   npts = amp.shape[0]
+    """
 
-   nAGC2=nAGC/2
-   ampAGC = np.zeros_like(amp)
-   absamp = np.zeros_like(amp)
+    # make sure input array is as expected:
+    amp = np.asarray(amp, dtype=np.float32)
 
-   absamp = np.abs(amp)
+    if len(amp.shape) != 1:
+        raise ValueError("amp must be a rank-1 array")
 
+    npts = amp.shape[0]
 
-   for i in xrange(nAGC2, npts - nAGC2):
-      fmax=0.0
-      for j in range(i-nAGC2,i+nAGC2+1):
+    nAGC2 = nAGC / 2
+    ampAGC = np.zeros_like(amp)
+    absamp = np.zeros_like(amp)
+
+    absamp = np.abs(amp)
+
+    for i in xrange(nAGC2, npts - nAGC2):
+        fmax = 0.0
+        for j in range(i - nAGC2, i + nAGC2 + 1):
             if absamp[j] > fmax:
                 fmax = absamp[j]
-      ampAGC[i] = amp[i]/fmax
-   
-   return ampAGC
+        ampAGC[i] = amp[i] / fmax
 
+    return ampAGC
