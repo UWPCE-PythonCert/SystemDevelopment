92b1e786dc85853b47b5b03df3e586a11cd4ae92
Chris Barker <PythonCHB@gmail.com>
2016-02-27 12:54:13 -0800

    made the build_gh_pages script a bit more robust

--- slides_sources/build_gh_pages.sh
+++ slides_sources/build_gh_pages.sh
@@ -3,19 +3,19 @@
 # simple script to build and push to gh-pages
 # designed to be run from master
 
-GHPAGESDIR=../../SystemDevelopment.gh-pages
+GHPAGESDIR=../../SystemDevelopment.gh-pages/
 
 # make sure the Gh pages repo is there and in the right branch
-pushd GHPAGESDIR
+pushd $GHPAGESDIR
 git checkout gh-pages
 popd
 
 # make the docs
 make html
 # copy to other repo (on the gh-pages branch)
-cp -R build/html/GHPAGESDIR
+cp -R build/html/ $GHPAGESDIR
 
-pushd GHPAGESDIR
+pushd $GHPAGESDIR
 git add * # in case there are new files added
 git commit -a -m "updating presentation materials"
 git pull -s ours --no-edit
