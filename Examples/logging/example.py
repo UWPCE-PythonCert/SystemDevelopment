fba70257d8de659a5508608abdf12a9393fa1e31
Chris Barker <PythonCHB@gmail.com>
2016-03-28 23:34:51 -0700

    created a more complex logging sample

---
+++ Examples/logging/example.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+"""
+Example code for using logging in a complex system
+"""
+import sys
+import logging
+import random
+import time
+import worker
+
+# The logging configuration:
+#  In real life, you might be pulling this from a config file, or ...
+
+# each level of logging gives more information
+# uncomment the level you want
+# log_level = logging.CRITICAL
+# log_level = logging.ERROR
+# log_level = logging.WARNING
+# log_level = logging.INFO
+log_level = logging.DEBUG
+
+# pretty simple format -- time stamp and the message
+# this can get fancy:
+#  https://docs.python.org/3/library/logging.html#logrecord-attributes
+format = '%(asctime)s %(levelname)s - %(module)8s - line:%(lineno)d - %(message)s'
+
+# configure the logger
+# basicConfig configures the "root logger" -- usually the one you want.
+# this sets it up to write to a file
+logging.basicConfig(filename='example.log',
+                    filemode='w',  # use 'a' if you want to preserve the old log file
+                    format=format,
+                    level=log_level)
+
+# and this will make it write to stdout as well
+# you would turn this off in production...
+if True:  # turn this off with False
+    std_handler = logging.StreamHandler(sys.stdout)
+    # give this a different formatter:
+    formatter = logging.Formatter('%(levelname)9s - %(module)s - %(message)s')
+    std_handler.setFormatter(formatter)
+    std_handler.setLevel(logging.INFO)
+    logging.getLogger().addHandler(std_handler)
+
+# Run the "application":
+while True:  # keep it running "forever"
+    # do something random:
+    logging.debug("calling a random worker")
+    random.choice(worker.workers)()
+    # wait a random length of time :
+    time.sleep(random.random())
--- Examples/logging/worker.py
+++ Examples/logging/worker.py
@@ -1,4 +1,51 @@
+"""
+A module with lots of random stuff that may happen
+
+This is designed to simulate a complex system --
+maybe event driven, etc.
+
+"""
+
 import logging
 
-def worker():
-    logging.info("worker called")
+import random
+
+
+def work1():
+    logging.info("work1 doing a job")
+
+
+def work2():
+    """
+    This one randomly logs a warning
+    """
+    logging.info("work2 doing a job")
+    if random.randint(1, 5) == 1:
+        logging.warning("something weird happened in work2!")
+
+
+def work3():
+    """
+    This one randomly logs an error
+    """
+    logging.info("work3 doing a job")
+    if random.randint(1, 5) == 1:
+        logging.error("Error in work3: bad input")
+
+
+called = [0]
+
+
+def work4():
+    """
+    Here something critical can go wrong and crash the system
+    """
+    logging.info("work4 doing something")
+    called[0] += 1
+    logging.debug("work4 called {:d} times".format(called[0]))
+    if called[0] >= 4:  # don't want it to crash right away :-)
+        if random.randint(1, 5) == 1:
+            logging.critical("Something went terribly wrong! Aborting!!!")
+            raise ValueError("got really, really bad input")
+
+workers = [work1, work2, work3, work4]
