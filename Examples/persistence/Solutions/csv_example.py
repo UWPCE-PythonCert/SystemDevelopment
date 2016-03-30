d7ad7df5fe3fbb50c74b0620070d6b7584420e5f
Chris Barker <PythonCHB@gmail.com>
2016-03-30 12:00:45 -0700

    started the process of porting soluitons to py3

--- Examples/persistence/Solutions/csv_example.py
+++ Examples/persistence/Solutions/csv_example.py
@@ -2,64 +2,67 @@
 
 """
 Example of how to save data as an CSV file with the CSV module
-
 """
-
-import csv
-
-outfilename = "add_book_data.csv"
 
 # get the data from the py file
 # csv format really only holds flat data well.
 from add_book_data_flat import AddressBook
 
+import csv
+
+outfilename = "add_book_data.csv"
+
 # create a csv file writing object
-writer = csv.writer( open(outfilename, 'wb') )
+writer = csv.writer(open(outfilename, 'w'))
 
 # write the headers
 # assume all data have the same keys
 headers = AddressBook[0].keys()
-writer.writerow ( headers )
+writer.writerow(headers)
 
 for person in AddressBook:
     row = [person[key] for key in headers]
-    writer.writerow(row)    
+    writer.writerow(row)
 
-del writer # to make sure the file gets closed
+del writer  # to make sure the file gets closed
 
-### see if it can be re-loaded.
+# see if it can be re-loaded.
 
 # create a csv file reading object
-reader = csv.reader( open(outfilename, 'rb') )
+reader = csv.reader(open(outfilename, 'r'))
+
 # read  the headers
-headers = reader.next() # it's an iterator -- so next() gives us the next row -- in this case, the first row
+# it's an iterator -- so next() gives us the next row -- in this case, the first row
+headers = next(reader)
 
 # build up the new version:
 AddressBook2 = []
 for row in reader:
     AddressBook2.append(dict(zip(headers, row)))
 
-del reader # to make sure the file is closed
+del reader  # to make sure the file is closed
 
-#Check if they are the same
+# Check if they are the same
 if AddressBook2 == AddressBook:
-    print "csv readr version is the same as the original"
+    print("csv reader version is the same as the original")
+else:
+    print("csv read version is NOT the same")
 
-## or use the built-in "DictReader":
-
+# or use the built-in "DictReader":
 # create a DictReader file reading object
-reader = csv.DictReader( open(outfilename, 'rb') )
+reader = csv.DictReader(open(outfilename, 'r'))
 # no need to read  the headers -- it will use the first row
 
 # build up the new version:
 AddressBook3 = []
 for row in reader:
-    print "row:", row
+    print("row:", row)
     AddressBook3.append(row)
 
-del reader # to make sure the file is closed
+del reader  # to make sure the file is closed
 
-#Check if they are the same
+# Check if they are the same
 if AddressBook3 == AddressBook:
-    print "The DictReader one is the the same"
-
+    print("The DictReader one is the the same")
+else:
+    print("The DictReader one is NOT the same!")
