b7bcfad58d86e2fe33135fa3f7b555ad5e679f57
Chris Barker <PythonCHB@gmail.com>
2016-04-28 11:43:23 -0700

    updated matplotlib notebooks and tweaks to all the numpy ones.

--- Examples/nosql/test_address_book_model.py
+++ Examples/nosql/test_address_book_model.py
@@ -6,12 +6,13 @@ if testing mongo, remember to start database first
 $ mongod --dbpath=mongo_data/
 """
 
-#import address_book_model as model
-#import address_book_zodb as model
+# import address_book_model as model
+# import address_book_zodb as model
 import address_book_mongo as model
 
 
 a_book = model.create_sample()
+
 
 def test_name_search():
     """find a single person by first name"""
@@ -22,6 +23,7 @@ def test_name_search():
     assert people[0].first_name == 'Chris'
     assert people[0].last_name == 'Barker'
 
+
 def test_name_search2():
     people = a_book.find_people('barKer')
     first_names = [p.first_name for p in people]
@@ -30,11 +32,13 @@ def test_name_search2():
     assert 'Emma' in first_names
     assert 'Donna' in first_names
 
+
 def test_zip_search():
     locations = a_book.find_zip_codes(98105)
 
     assert len(locations) == 1
     assert locations[0].name == 'Python Certification Program'
+
 
 def test_state_search():
     locations = a_book.find_state('WA')
@@ -42,5 +46,3 @@ def test_state_search():
 
     assert "The Barkers" in names
     assert "Python Certification Program" in names
-
-
