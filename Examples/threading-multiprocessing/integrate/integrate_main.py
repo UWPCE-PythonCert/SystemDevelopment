b04cf6821259519c1ff0cee00b225180b325892c
Chris Barker <PythonCHB@gmail.com>
2016-05-10 18:23:06 -0700

    updated integrate example for py3

--- Examples/threading-multiprocessing/integrate/integrate_main.py
+++ Examples/threading-multiprocessing/integrate/integrate_main.py
@@ -6,6 +6,6 @@ from integrate import integrate, f
 a = 0.0
 b = 10.0
 
-for N in (10**i for i in xrange(1,8)):
-    print "Numerical solution with N=%(N)d : %(x)f" % \
-        {'N': N, 'x': integrate(f, a, b, N)}
+for N in (10**i for i in range(1, 8)):
+    print("Numerical solution with N=%(N)d : %(x)f" %
+          {'N': N, 'x': integrate(f, a, b, N)})
