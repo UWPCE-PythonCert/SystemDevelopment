429558296dfc1e68c27edca0e56d5276af4b04f8
Chris Barker <PythonCHB@gmail.com>
2016-05-10 19:06:30 -0700

    fixed some style issues

--- Examples/threading-multiprocessing/lock/stdout_writer.py
+++ Examples/threading-multiprocessing/lock/stdout_writer.py
@@ -3,14 +3,14 @@ import sys
 import threading
 import time
 
+
 def write():
-    sys.stdout.write( "%s writing.." % threading.current_thread().name)
+    sys.stdout.write("%s writing.." % threading.current_thread().name)
     time.sleep(random.random())
-    sys.stdout.write( "..done\n")
+    sys.stdout.write("..done\n")
 
 for i in range(100):
     thread = threading.Thread(target=write)
-    thread.daemon = True # allow ctrl-c to end
+    thread.daemon = True  # allow ctrl-c to end
     thread.start()
     time.sleep(.1)
-
