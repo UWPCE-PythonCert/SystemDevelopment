3b8f30e481c2e1fc1ba334ca91d6f5cc006f1f14
Chris Barker <PythonCHB@gmail.com>
2016-05-10 19:17:52 -0700

    updated for py3

--- Examples/threading-multiprocessing/decorators/decorators.py
+++ Examples/threading-multiprocessing/decorators/decorators.py
@@ -2,26 +2,29 @@
 
 import time
 
+
 def timer(func):
     def timer(*args, **kwargs):
         t1 = time.time()
         result = func(*args, **kwargs)
-        print "-- executed in %.4f seconds :" % (time.time() - t1),
+        print("-- executed in %.4f seconds :" % (time.time() - t1))
         return result
     return timer
 
+
 def logger(func):
     def logger(*args, **kwargs):
-        print "\n\n-- calling function\n\n"
+        print("\n\n-- calling function\n\n")
         func(*args, **kwargs)
-        print "\n\n-- function call succeeded\n\n"
+        print("\n\n-- function call succeeded\n\n")
     return logger
+
 
 def exception_handler(func):
     def handle_exceptions(*args, **kwargs):
         try:
-            print "handling exceptions.."
+            print("handling exceptions..")
             func(*args, **kwargs)
         except Exception as e:
-            print "\n\n-- Received exception, logging message: %s\n\n" % str(e)
+            print("\n\n-- Received exception, logging message: %s\n\n" % str(e))
     return handle_exceptions
--- Examples/threading-multiprocessing/threading/integrate_main.py
+++ Examples/threading-multiprocessing/threading/integrate_main.py
@@ -4,33 +4,35 @@ import argparse
 import os
 import sys
 import threading
-import Queue
+import queue
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
 from integrate.integrate import integrate, f
 from decorators.decorators import timer
+
 
 @timer
 def threading_integrate(f, a, b, N, thread_count=2):
     """break work into two chunks"""
     N_chunk = int(float(N) / thread_count)
-    dx = float(b-a) / thread_count
+    dx = float(b - a) / thread_count
 
-    results = Queue.Queue()
+    results = queue.Queue()
 
     def worker(*args):
         results.put(integrate(*args))
 
     threads = []
-    for i in xrange(thread_count):
-        x0 = dx*i
+    for i in range(thread_count):
+        x0 = dx * i
         x1 = x0 + dx
         thread = threading.Thread(target=worker, args=(f, x0, x1, N_chunk))
         thread.start()
-        print "Thread %s started" % thread.name
+        print("Thread %s started" % thread.name)
         # thread1.join()
 
-    return sum( (results.get() for i in xrange(thread_count) ))
+    return sum((results.get() for i in range(thread_count)))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='integrator')
@@ -45,6 +47,6 @@ if __name__ == "__main__":
     N = args.N
     thread_count = args.thread_count
 
-    print "Numerical solution with N=%(N)d : %(x)f" % \
-            {'N': N, 'x': threading_integrate(f, a, b, N, thread_count=thread_count)}
+    print("Numerical solution with N=%(N)d : %(x)f" %
+          {'N': N, 'x': threading_integrate(f, a, b, N, thread_count=thread_count)})
 
