f155f6ec85ee06d677022761b3817ac160cdab11
Chris Barker <PythonCHB@gmail.com>
2016-05-10 20:22:18 -0700

    fixed for py3

--- Examples/threading-multiprocessing/server/app.py
+++ Examples/threading-multiprocessing/server/app.py
@@ -3,9 +3,9 @@ from itertools import count
 i = count()
 
 def app(environ, start_response):
-  data = "Hello, World. %d\n" % i.next()
+  data = "Hello, World. %d\n" % next(i)
   start_response("200 OK", [
       ("Content-Type", "text/plain"),
       ("Content-Length", str(len(data)))
   ])
-  return iter([data])
+  return iter([data.encode('utf-8')])
--- Examples/threading-multiprocessing/server/client-threading.py
+++ Examples/threading-multiprocessing/server/client-threading.py
@@ -2,8 +2,8 @@
 
 import os
 import sys
-import urllib2
-import threading, Queue
+import urllib.request
+import threading, queue
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
@@ -12,24 +12,24 @@ from decorators.decorators import timer
 @timer
 def threading_client(number_of_requests=10):
 
-    results = Queue.Queue()
+    results = queue.Queue()
     url = "http://localhost:37337"
 
     def worker(*args):
-        conn = urllib2.urlopen(url)
+        conn = urllib.request.urlopen(url)
         result = conn.read()
         conn.close()
         results.put(result)
 
-    for i in xrange(number_of_requests):
+    for i in range(number_of_requests):
         thread = threading.Thread(target=worker, args=())
         thread.start()
-        print "Thread %s started" % thread.name
+        print("Thread %s started" % thread.name)
 
-    for i in xrange(number_of_requests):
-        print results.get(timeout=2)
+    for i in range(number_of_requests):
+        print(results.get(timeout=2))
 
-    print "made %d requests" % number_of_requests
+    print("made %d requests" % number_of_requests)
 
 if __name__ == "__main__":
 
